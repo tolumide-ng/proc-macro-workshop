f8d7624134382da22380ceb995810766567009c2
tolumide-ng <tolumideshopein@gmail.com>
2023-05-21 21:37:25 +0200

    Seq: Test 5

--- seq/src/approach1.rs
+++ seq/src/approach1.rs
@@ -1,3 +1,7 @@
+use std::result;
+
+use quote::ToTokens;
+
 
 
 #[derive(Debug)]
@@ -6,6 +10,13 @@ pub struct SeqMacroInput {
     to: usize,
     tt: proc_macro2::TokenStream,
     ident: syn::Ident
+}
+
+
+#[derive(Debug, Copy, Clone)]
+enum Mode {
+    RepalceIdent(usize),
+    ReplaceSequence,
 }
 
 
@@ -19,6 +30,7 @@ impl syn::parse::Parse for SeqMacroInput {
         let to_litint = syn::LitInt::parse(input)?;
         let to = to_litint.base10_parse::<usize>()?;
 
+        
         let content;
         syn::braced!(content in input);
         let tt = proc_macro2::TokenStream::parse(&content)?;
@@ -29,58 +41,78 @@ impl syn::parse::Parse for SeqMacroInput {
 
 impl Into<proc_macro::TokenStream> for SeqMacroInput {
     fn into(self) -> proc_macro::TokenStream {
-        (self.from..self.to).map(|i| -> proc_macro::TokenStream {
-            self.expand_stream(self.tt.clone(), i).into()
-        }
-        )
-            .collect::<proc_macro::TokenStream>()
+        self.expand(self.tt.clone()).into()
     }
 }
 
 
 impl SeqMacroInput {
-    fn expand_stream(&self, stream: proc_macro2::TokenStream, i: usize) -> proc_macro2::TokenStream {
+   fn expand(&self, stream: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+        let (out, mutated) = self.expand_stream(stream.clone(), Mode::ReplaceSequence);
+        if mutated {
+            return out
+        }
 
-        let mut tst = stream.into_iter();
+        (self.from..self.to).map(|i| {
+            self.expand_stream(stream.clone(), Mode::RepalceIdent(i)).0
+        }).collect::<proc_macro2::TokenStream>()
+    }
+
+
+    fn expand_stream(&self, stream: proc_macro2::TokenStream, mode: Mode
+        ) -> (proc_macro2::TokenStream, bool) {
         let mut output = proc_macro2::TokenStream::new();
+        let mut mutated = false;
+        let mut tst = stream.into_iter();
 
         while let Some(tt) = tst.next() {
-            let result: proc_macro2::TokenStream = self.expand_tt(tt, &mut tst, i).into();
+            let result: proc_macro2::TokenStream = self.expand_tt(tt, &mut tst, &mut mutated, mode);
             output.extend(result);
         }
 
-        output
+        (output, mutated)
     }
     
     /// Expands the token tree
-    fn expand_tt(&self, tt: proc_macro2::TokenTree, rest: &mut proc_macro2::token_stream::IntoIter, i: usize) -> proc_macro2::TokenTree {
-        match tt {
+    fn expand_tt(
+        &self, tt: proc_macro2::TokenTree, rest: &mut proc_macro2::token_stream::IntoIter, 
+        mutated: &mut bool, mode: Mode,
+    ) -> proc_macro2::TokenStream {
+        let tt = match tt {
          proc_macro2::TokenTree::Group(g) => {
-            let mut expanded = proc_macro2::Group::new(g.delimiter(), self.expand_stream(g.stream(), i));
-            expanded.set_span(g.span());
+            let (expanded, g_mutated) = self.expand_stream(g.stream(), mode);
+            let mut expanded = proc_macro2::Group::new(g.delimiter(), expanded);
+            *mutated |= g_mutated;
 
-            proc_macro2::TokenTree::Group(expanded)
+            expanded.set_span(g.span());
+            proc_macro2::TokenTree::Group(expanded).into()
+         }        
+         proc_macro2::TokenTree::Ident(ident) if ident == self.ident => {
+            if let Mode::RepalceIdent(i) = mode {
+                let mut lit = proc_macro2::Literal::usize_unsuffixed(i);
+                lit.set_span(ident.span());
+                *mutated = true;
+    
+                let t = syn::parse2(quote::quote_spanned! {ident.span()=> #lit}).unwrap();
+                proc_macro2::TokenTree::Literal(t)
+            } else {
+                proc_macro2::TokenTree::Ident(ident)
+            }
          }
          
-         proc_macro2::TokenTree::Ident(ref ident) if ident == &self.ident => {
-            let mut lit = proc_macro2::Literal::usize_unsuffixed(i);
-            lit.set_span(ident.span());
-
-            let t = syn::parse2(quote::quote_spanned! {ident.span()=> #lit}).unwrap();
-            proc_macro2::TokenTree::Literal(t)
-         }
-
-
          proc_macro2::TokenTree::Ident(mut ident) => {
             let mut peek = rest.clone();
 
-            match (peek.next(), peek.next()) {
-                (Some(proc_macro2::TokenTree::Punct(ref punct)), Some(proc_macro2::TokenTree::Ident(ref ident2)))
-                if punct.as_char() == '~' && ident2 == &self.ident => {
+            match (mode, peek.next(), peek.next()) {
+                (
+                    Mode::RepalceIdent(i), 
+                    Some(proc_macro2::TokenTree::Punct(ref punct)), 
+                    Some(proc_macro2::TokenTree::Ident(ref ident2))
+                ) if punct.as_char() == '~' && ident2 == &self.ident => {
                     let value = format!("{}{}", ident, i);
-
                     ident = proc_macro2::Ident::new(value.as_str(), ident.span());
                     *rest = peek.clone();
+                    *mutated = true;
 
 
                     match (peek.next(), peek.next()) {
@@ -97,11 +129,38 @@ impl SeqMacroInput {
                 }
                 _ => {}
             }
-
             proc_macro2::TokenTree::Ident(ident)
          }
+        
+        // this expands scenarios such as on test5: where the par to be repeated is wrapped as a group
+         proc_macro2::TokenTree::Punct(punct) if punct.as_char() == '#' => {
+             
+             if let Mode::ReplaceSequence = mode {
+                let mut peek = rest.clone();
+                match (peek.next(), peek.next()) {
+                    (   
+                        Some(proc_macro2::TokenTree::Group(ref g)), 
+                        Some(proc_macro2::TokenTree::Punct(ref star))
+                    )
+                    if g.delimiter() == proc_macro2::Delimiter::Parenthesis && star.as_char() == '*' => 
+                    {
+                        *mutated = true;
+                        *rest = peek;
+
+
+                        return (self.from..self.to).map(|i| {
+                            self.expand_stream(g.stream(), Mode::RepalceIdent(i))
+                        }).map(|(ts, _)| ts).collect::<proc_macro2::TokenStream>()
+                    }
+                    _ => {}
+                }
+            }
+            proc_macro2::TokenTree::Punct(punct)
+         }
          tt => tt
-        }
+        };
+
+        std::iter::once(tt).collect()
     }
 }
 
--- seq/src/main.rs
+++ seq/src/main.rs
@@ -1,75 +1,47 @@
-// One of the big things callers will want to do with the sequential indices N
-// is use them as part of an identifier, like f0 f1 f2 etc.
+// So far our macro has repeated the entire loop body. This is not sufficient
+// for some use cases because there are restrictions on the syntactic position
+// that macro invocations can appear in. For example the Rust grammar would not
+// allow a caller to write:
 //
-// Implement some logic to paste together any Ident followed by `~` followed by
-// our loop variable into a single concatenated identifier.
-//
-// The invocation below will expand to:
-//
-//     fn f1() -> u64 { 1 * 2 }
-//     fn f2() -> u64 { 2 * 2 }
-//     fn f3() -> u64 { 3 * 2 }
-//
-// Optionally, also support more flexible arrangements like `f~N~_suffix` ->
-// f0_suffix f1_suffix etc, though the test suite only requires `prefix~N` so
-// you will need to add your own tests for this feature.
-//
-//
-// Resources:
-//
-//     - Example of creating a new Ident from a string:
-//       https://docs.rs/syn/1.0/syn/struct.Ident.html
-
-
-
-// use seq::seq;
-
-// seq!(N in 1..4 {
-//     fn f~N () -> u64 {
-//         N * 2
+//     enum Interrupt {
+//         seq!(N in 0..16 {
+//             Irq~N,
+//         });
 //     }
-// });
-
-// // This f0 is written separately to detect whether your macro correctly starts
-// // with the first iteration at N=1 as specified in the invocation. If the macro
-// // incorrectly started at N=0 like in the previous tests cases, the first
-// // generated function would conflict with this one and the program would not
-// // compile.
-// fn f0() -> u64 {
-//     100
-// }
-
-// fn main() {
-//     let sum = f0() + f1() + f2() + f3();
-
-//     assert_eq!(sum, 100 + 2 + 4 + 6);
-// }
-
-
-
+//
+// because this is just not a legal place to put a macro call.
+//
+// Instead we will implement a way for the caller to designate a specific part
+// of the macro input to be repeated, so that anything outside that part does
+// not get repeated. The repeated part will be written surrounded by #(...)*.
+//
+// The invocation below should expand to:
+//
+//     #[derive(Copy, Clone, PartialEq, Debug)]
+//     enum Interrupt {
+//         Irq0,
+//         ...
+//         Irq15,
+//     }
+//
+// Optionally, allow for there to be multiple separate #(...)* sections,
+// although the test suite does not exercise this case. The #(...)* sections
+// will each need to be repeated according to the same loop bounds.
 
 use seq::seq;
 
-
-
-// This f0 is written separately to detect whether your macro correctly starts
-// with the first iteration at N=1 as specified in the invocation. If the macro
-// incorrectly started at N=0 like in the previous tests cases, the first
-// generated function would conflict with this one and the program would not
-// compile.
-
-seq!(N in 1..4 {
-    fn f~N~_suffix () -> u64 {
-        N * 2
+seq!(N in 0..16 {
+    #[derive(Copy, Clone, PartialEq, Debug)]
+    enum Interrupt {
+        #(
+            Irq~N,
+        )*
     }
 });
 
-fn f0_suffix() -> u64 {
-    100
-}
-
 fn main() {
-    let sum = f0_suffix() + f1_suffix() + f2_suffix() + f3_suffix();
+    let interrupt = Interrupt::Irq8;
 
-    assert_eq!(sum, 100 + 2 + 4 + 6);
+    assert_eq!(interrupt as u8, 8);
+    assert_eq!(interrupt, Interrupt::Irq8);
 }
